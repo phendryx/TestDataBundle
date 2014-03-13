2854863c4396fccd93df10049bbc568738376fe1
Paul Hendryx <phendryx@malwarebytes.org>
2014-03-13 11:29:05 -0700

    getting config in listener

--- Importer/Importer.php
+++ Importer/Importer.php
@@ -6,9 +6,10 @@ use Malwarebytes\TestBundle\Event\ImportDataEvent;
 
 class Importer
 {
-
     public function onImportEvent(ImportDataEvent $event)
     {
+        $params = $event->getContainer()->getParameter('malwarebytes_test_data.config');
+        \Doctrine\Common\Util\Debug::dump($params);
 
         echo "here!";
     }

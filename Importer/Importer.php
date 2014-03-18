9babb968bd075d4d84878ff9a0b9285fbe19cd82
Paul Hendryx <phendryx@malwarebytes.org>
2014-03-18 12:08:34 -0700

    importer now imports doctorine associated entities

--- Importer/Importer.php
+++ Importer/Importer.php
@@ -6,28 +6,31 @@ use Doctrine\ORM\EntityManager;
 
 use Ddeboer\DataImport\Workflow;
 use Ddeboer\DataImport\Reader\CsvReader;
-use Ddeboer\DataImport\Writer\DoctrineWriter;
+use Malwarebytes\TestDataBundle\DataImport\Writer\DoctrineWriter;
 use Ddeboer\DataImport\ValueConverter\DateTimeValueConverter;
 use Malwarebytes\TestBundle\Event\ImportDataEvent;
 use Malwarebytes\TestDataBundle\Data\Factory\DataFactory;
 use Malwarebytes\TestDataBundle\Data\Data as TestData;
+use Ddeboer\DataImport\ValueConverter\CallbackValueConverter;
+use Ddeboer\DataImport\ValueConverter\ArrayValueConverterMap;
+use Ddeboer\DataImport\ItemConverter\CallbackItemConverter;
+use ReflectionClass;
+use Ddeboer\DataImport\Filter\CallbackFilter;
+use Malwarebytes\TestDataBundle\Data\Mapping;
 
 class Importer
 {
     public function onImportEvent(ImportDataEvent $event)
     {
-        $dataConfig = $event->getContainer()->getParameter('malwarebytes_test_data.config');
+        $config = $event->getContainer()->getParameter('malwarebytes_test_data.config');
         $rootDirectory = $event->getContainer()->get('kernel')->getRootDir();
-        $testDataDirectory = $rootDirectory . $dataConfig['test_data_directory'];
+        $testDataDirectory = $rootDirectory . $config['test_data_directory'];
         $eventManager = $event->getContainer()->get('doctrine')->getManager();
 
         $factory = new DataFactory();
-        foreach($dataConfig['objects'] as $name => $config) {
-            if ($name != 'user') {
-                continue;
-            }
+        foreach($config['objects'] as $name => $objectConfig) {
 
-            $testData = $factory->getNewData($config);
+            $testData = $factory->getNewData($objectConfig);
             $this->import($testDataDirectory, $eventManager, $name, $testData);
         }
     }
@@ -38,6 +41,7 @@ class Importer
 
         // Create and configure the reader
         $file = new \SplFileObject($filePath);
+//        $file->setCsvControl(",");
         $csvReader = new CsvReader($file);
 
         // Tell the reader that the first row in the CSV file contains column headers
@@ -52,10 +56,34 @@ class Importer
 
         // Add a converter to the workflow that will convert `beginDate` and `endDate`
         // to \DateTime objects
-//        $dateTimeConverter = new DateTimeValueConverter('Ymd');
-//        $workflow
-//            ->addValueConverter('beginDate', $dateTimeConverter)
-//            ->addValueConverter('endDate', $dateTimeConverter);
+        if ($name == "post") {
+            $dateTimeConverter = new DateTimeValueConverter('Y-m-d');
+            $workflow
+                ->addValueConverter('date', $dateTimeConverter);
+        }
+
+        // Create the item converter function that will associate an entity with this row of data
+        $converter = new CallbackItemConverter(function ($item) use ($entityManager, $testData) {
+            if (count($testData->getMappings()) > 0) {
+                foreach ($testData->getMappings() as $mapping) {
+                    // Get the repository for the entity
+                    $repository = $entityManager->getRepository($mapping->getEntity());
+
+                    // Get the entity from doctrine
+                    $entity = $repository->find($item[$mapping->getDataField()]);
+
+                    // Set the property to the found entity
+                    $item[$mapping->getProperty()] = $entity;
+                }
+            }
+
+            // Return data item
+            return $item;
+        });
+
+        // Add the item converter to the workflow
+        $workflow->addItemConverter($converter);
+
 
         // Process the workflow
         $workflow->process();
